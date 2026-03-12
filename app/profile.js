403f81f24d1a3895654fb7d9a80953dcf1507e6d
fineigor6-debug <fineigor6@gmail.com>
2026-03-13 02:38:13 +0400

    Update profile.js

--- app/profile.js
+++ app/profile.js
@@ -1,1 +1,85 @@
+const tg = window.Telegram.WebApp
+tg.expand()
 
+const avatar = document.getElementById("avatar")
+const name = document.getElementById("name")
+const opened = document.getElementById("opened")
+
+function goHome(){
+window.location.href = "index.html"
+}
+
+/* TELEGRAM USER */
+
+const user = tg.initDataUnsafe?.user
+
+if(user){
+
+name.innerText = user.first_name + (user.username ? " @" + user.username : "")
+
+if(user.photo_url){
+avatar.src = user.photo_url
+}
+
+}
+
+/* OPENED CASES */
+
+opened.textContent = localStorage.getItem("openedCases") || 0
+
+
+/* =========================
+   TESTER BALANCE
+========================= */
+
+function openBalanceTester(){
+
+document
+.getElementById("testerPopup")
+.classList
+.add("active")
+
+}
+
+function closeTester(){
+
+document
+.getElementById("testerPopup")
+.classList
+.remove("active")
+
+}
+
+function giveTestBalance(){
+
+const input = document.getElementById("testerAmount")
+
+let value = parseInt(input.value)
+
+if(!value || value < 1){
+
+showNotice("Введите сумму")
+return
+
+}
+
+if(value > 100){
+
+showNotice("Максимум 100")
+return
+
+}
+
+let balance = getBalance()
+
+balance += value
+
+setBalance(balance)
+
+showNotice("Выдано +" + value)
+
+closeTester()
+
+input.value = ""
+
+}
